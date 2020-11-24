85884c75a9ddc8955a9accd1a62160b739755e7b
SaniaKhan159 <44388226+SaniaKhan159@users.noreply.github.com>
2020-11-23 16:29:07 -0800

    updated message4

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
         <h2>My first App</h2>
-      </header>
     </div>
   );
 }
